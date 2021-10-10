08b5f886dd4d8408ceb3831bdba01cbb37338b10
Tina Maria Thomas <tinamariathomas@gmail.com>
2021-10-10 00:40:51 -0400

    Added react native chart kit

--- ExpenseSummary.tsx
+++ ExpenseSummary.tsx
@@ -1,11 +1,75 @@
 import * as React from 'react';
+import {
+  SafeAreaView,
+  Dimensions
+} from 'react-native';
 
-import { SafeAreaView, Text, TextInput } from 'react-native';
-export default function ExpenseSummary() {
+import {
+  PieChart
+} from 'react-native-chart-kit';
 
-  return (
+
+const screenWidth = Dimensions.get("window").width;
+
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5
+};
+
+const data = [
+  {
+    name: "Groceries",
+    population: 150,
+    color: "rgba(131, 167, 234, 1)",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "Snacks",
+    population: 30,
+    color: "#F00",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "Eating out",
+    population: 40,
+    color: "red",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "Household",
+    population: 50,
+    color: "#ffffff",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  },
+  {
+    name: "Breakfast",
+    population: 10,
+    color: "rgb(0, 0, 255)",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15
+  }
+];
+
+const ExpenseSummary = () =>
     <SafeAreaView>
-        <Text>Hello</Text>
-    </SafeAreaView>
-  );
-}
+          <PieChart
+            data={data}
+            width={screenWidth}
+            height={220}
+            chartConfig={chartConfig}
+            accessor="population"
+            backgroundColor="transparent"
+            paddingLeft="15"
+          />
+    </SafeAreaView>;
+
+export default ExpenseSummary;
